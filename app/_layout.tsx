044d6e2748b10d6a62bff247522ab95af42cc6d4
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2025-01-26 03:07:04 +0530

    Remove unused font loading logic from RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,6 @@
 import { Stack } from "expo-router";
 import "./global.css"
-import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-
-  const [fontsLoaded] = useFonts(map:{
-    
-  })
   return <Stack />;
 }
